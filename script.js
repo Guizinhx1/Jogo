c40c332659d81f68336a1864aea7df44295bb078
GUILHERME DE MATOS SILVA <guilherme_silva95@estudante.sesisenai.org.br>
2025-02-27 16:55:57 -0300

    'atualização do jogo

--- script.js
+++ script.js
@@ -1,106 +1,131 @@
-const canvas = document.getElementById("jogoCanvas")
+const canvas = document.getElementById('jogoCanvas')
 const ctx = canvas.getContext('2d')
-let gravidade = 0.5
+let gameOver = false
+let pontuacao = 0
 
-document.addEventListener("click", (e) =>{
-    if(gameOver){
+class Entidade {
+    #gravidade
+    constructor(x,y,largura,altura){
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura=altura
+        this.#gravidade=0.5
+    }
+    desenhar = function (ctx, cor) {
+        ctx.fillStyle = cor
+        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+    }
+    getGravidade() {
+        return this.#gravidade;
+    }
+}
+class Personagem extends Entidade{
+    #velocidadey
+    #pulando
+    constructor(x,y,largura,altura){
+        super(x,y,largura,altura);
+        this.#velocidadey=0
+        this.#pulando = false
+    }
+    saltar = function(){
+        this.#velocidadey -= 15
+        this.#pulando = true
+    }
+    isPersonagemPulando = function(){
+        return this.#pulando
+    }
+    atualizar = function(){
+        if(this.#pulando == true){
+            this.#velocidadey += this.getGravidade()
+            this.y += this.#velocidadey
+            if(this.y>=canvas.height-50){
+                this.#velocidadey = 0
+                this.#pulando=false
+            }
+        }
+    }
+    
+    verificarColisao = function(obstaculo){
+        if(
+            this.x < obstaculo.x + obstaculo.largura &&
+            this.x + this.largura > obstaculo.x &&
+            this.y < obstaculo.y + obstaculo.altura &&
+            this.y + this.altura > obstaculo.y
+        ){  
+            this.#houveColisao(obstaculo)
+        }
+    }
+    #houveColisao = function (obstaculo){
+        obstaculo.pararObstaculo()
+        obstaculo.atualizar()
+        ctx.fillStyle='black'
+        ctx.fillRect((canvas.width/2)-200,(canvas.height/2)-50,400,100)
+        ctx.fillStyle='white'
+        ctx.font="50px Arial"
+        ctx.fillText("GAME OVER",(canvas.width/2)-150,(canvas.height/2))
+        ctx.font = "30px Arial";
+        ctx.fillText("Pontuação: " + pontuacao, (canvas.width / 2) - 100, (canvas.height / 2) + 40);
+        gameOver=true
+    }
+    desenhar = function (){
+        ctx.drawImage(document.getElementById("mf"), this.x, this.y, this.largura, this.altura )
+    }
+
+}
+class Obstaculo extends Entidade{
+    #velocidadex
+    constructor(x,y,largura,altura){
+        super(x,y,largura,altura);
+        this.#velocidadex=7
+    }
+    getVelocidadeX = function () {
+        return this.#velocidadex
+    }
+    atualizar = function(){
+        this.x -= this.getVelocidadeX()
+        if (this.x <= 0-this.largura){
+            this.x = canvas.width-100
+            let altura_random = (Math.random() * 50)+90
+            this.altura = altura_random
+            this.y = canvas.height - altura_random
+            this.velocidadex += 0.5
+            pontuacao++;
+        }
+    }
+    pararObstaculo = function () {
+        this.#velocidadex = 0
+    }
+}
+
+const obstaculo = new Obstaculo(canvas.width-100,canvas.height-100,50,100)
+const personagem = new Personagem(50, canvas.height-50, 50, 50)
+
+document.addEventListener("click", (e) => {
+    if (gameOver==true){
         location.reload()
     }
 })
 
 document.addEventListener('keypress', (e) =>{
-    if (e.code == 'Space' && personagem.pulando == false){
-        personagem.velocidadey = -15
-        personagem.pulando = true
+    if (e.code == 'Space' && personagem.isPersonagemPulando() == false){
+        personagem.saltar()
     }
 })
-    
 
-let gameOver = false
+function loop () {
+    ctx.clearRect(0,0,canvas.width, canvas.height)
 
-const personagem = {
-    posx: 50,
-    posy: canvas.height -50,
-    tamx: 50,
-    tamy: 50,
-    velocidade: 0,
-    velocidadey: 0,
-    pulando: false, 
-    onground: true
-}
-function desenhaPersonagem(){
-    ctx.fillStyle = 'white'
-    ctx.fillRect(personagem.posx, personagem.posy, personagem.tamx, personagem.tamy)
-}
-
-function atualizaPersonagem (){
-    if (personagem.pulando){
-        personagem.velocidadey += gravidade
-        personagem.posy += personagem.velocidadey
-    }
-}
-
-function chao(){
-    if(personagem.posy == canvas.height -50){
-        personagem.velocidadey = 0
-        personagem.pulando = false
-        personagem.onground = true
-    }
-}
-
-const obstaculo = {
-    posx: canvas.width -100,
-    posy: canvas.height -100,
-    tamx: 50,
-    tamy: 100,
-    velocidade: 10
-}
-function desenhaObstaculo(){
-    ctx.fillStyle = 'red'
-    ctx.fillRect(obstaculo.posx, obstaculo.posy, obstaculo.tamx, obstaculo.tamy)
-}
-function atualizaObstaculo(){
-    obstaculo.posx -= obstaculo.velocidade
-    if(obstaculo.posx <= 0 - obstaculo.tamx){
-      let tamy_random = (Math.random() * 50) + 90
-       obstaculo.posx = canvas.width - 100
-       obstaculo.tamy = tamy_random
-       obstaculo.posy = canvas.height - tamy_random
-       obstaculo.velocidade += 0.5
-    }
-}
-
-function verificaColisao(){
-    if(personagem.posx < obstaculo.posx + obstaculo.tamx &&
-        personagem.posx + personagem.tamx > obstaculo.posx &&
-        personagem.posy < obstaculo.posy + obstaculo.tamy &&
-        personagem.posy + personagem.tamy > obstaculo.posy
-    ){
-        houveColisao()    
-    }
-}
-
-function houveColisao(){
-    obstaculo.velocidade = 0
-        atualizaObstaculo()
-    ctx.fillStyle = 'red'
-    ctx.fillRect((canvas.width/2)-200, (canvas.height/2)-50, 400, 100)
-    ctx.fillStyle='black'
-    ctx.font="50px Arial"
-    ctx.fillText("Game Over", (canvas.width/2)-130, (canvas.height/2) +15)
-    gameOver = true
-}
-
-function loop(){
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-
-    atualizaObstaculo()
-    desenhaObstaculo()
-    atualizaPersonagem()
-    desenhaPersonagem()
-    chao()
-    verificaColisao()
+    obstaculo.desenhar(ctx, 'green')
+    personagem.desenhar()
+    personagem.verificarColisao(obstaculo)
+    obstaculo.atualizar()
+    personagem.atualizar()
     requestAnimationFrame(loop)
 }
 
 loop()
+
+// adicionar multiplos objetos
+// adicionar pontuação
+// aplicar polimorfismo para mudar o desenho do personagem
